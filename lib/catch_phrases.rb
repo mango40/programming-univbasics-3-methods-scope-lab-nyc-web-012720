965a347b6834b5de91a64ffaf1fa3dcf204e8492
Manoj Patel <nycdays300@gmail.com>
2020-01-20 23:10:05 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,5 @@
-$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
-puts status
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase = "It's-a me, Mario!"
 end
 
